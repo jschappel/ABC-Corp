c264296c6f5caffee3ed4122ff5c66310fc5bbcb
jschappel <jmschappel12@gmail.com>
2019-06-12 14:12:00 -0400

    Fixed warning
    Interface resolver warning is now being ignored.

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,23 +1,29 @@
-/**
- * Server setup in this file
- */
-
 const express = require('express')
-const { ApolloServer } = require('apollo-server-express')
+const { ApolloServer, makeExecutableSchema } = require('apollo-server-express')
 const resolvers = require('./Graphql/Resolvers/index')
-const models = require('./SQL/index')
-const schema = require('./Graphql/Schemas/index')
+//const models = require('./SQL/index')
+const typeDefs = require('./Graphql/Schemas/index')
 
 const app = express()
+
+/**
+ * Makes a GraphQLSchema instanced schema
+ */
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+    resolverValidationOptions: {
+		requireResolversForResolveType: false,
+	},
+})
 
 /**
  * Create an Apollo server and supply it with the schema and resolvers
  * context: option field which can be used to used to inject dependencies from the outside to the resolver function.
  *      EX: A signed in users id could be set to the context to all future queries can access it.
- */
- const server = new ApolloServer({
-    typeDefs: schema,
-    resolvers,
+*/
+const server = new ApolloServer({
+    schema,
     /* context: {
         models, // Models is passed to the resolvers as context
         me: models.users[1] // Simulates a user that is logged in
