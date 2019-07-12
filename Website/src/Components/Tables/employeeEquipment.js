9544fb117077d0712b8075e9155948f1a02d6a96
Joshua Schappel <jmschappel12@gmail.com>
2019-07-12 13:33:01 -0400

    Sorting headers almost done
    - Sort function finished just need to rebase it to the header component

--- Website/src/Components/Tables/employeeEquipment.js
+++ Website/src/Components/Tables/employeeEquipment.js
@@ -4,18 +4,53 @@ import SortHeader from './TableComponents/sortedTableHeader'
 
 class EquipmentTable extends Component{
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: this.props.data.sort(this.compare('model', 'acs'))
+        }
+
+        this.sortTable = this.sortTable.bind(this)
+    }
+
     /**
      * sortTable: Sorts a table (ASC or DESC) by a specified column.
      * @param {String} id The object id to sort by
      * @param {Boolean} boolean true if sort ASC, false if sort DESC
      * @returns {Array} an array containing the sorted Objects by the specified id.
      */
-    sortTable(id, boolean){
-        console.log(`sorting table by ${id} ${boolean ? 'ASC' : 'DESC'}`)
+    async sortTable(compareKey, boolean){
+        const result = await this.state.data.sort(this.compare(compareKey, boolean ? 'asc' : 'desc'))
+        this.setState({
+            data: result
+        })
+    }
+
+    /**
+     * compare: Compares objects by a specified key for sorting.
+     * @param {Sting} key the Object key to sort by
+     * @param {Sting} order 'acs' to sort by ascending, 'desc' to sort by descending
+     * @returns{Integer} -1 if second element goes before first, 1 if first element goes before second, 0
+     *      if both elements are the same.
+     */
+    compare(key, order) {
+        return function(a, b) {
+            // Both objects do not have the same key so exit
+            if(!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) return 0
+            // see if they are strings, if so then convert to uppercase for comparison
+            const newA = typeof a[key] === 'string' ? a[key].toUpperCase() : a[key]
+            const newB = typeof b[key] === 'string' ? b[key].toUpperCase() : b[key]
+
+            // compare the values
+            let comparison = 0
+            if (newA > newB) comparison = 1
+            else if( newA < newB) comparison = -1
+
+            return(order === 'desc' ? (comparison * -1) : comparison)
+        }
     }
 
     render() {
-        console.log(this.props.data)
         return(
             <div className="container mt-3">
                 <h5>Lent Equipment</h5>
